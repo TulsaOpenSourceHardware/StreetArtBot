e22c56d6d3a877359ee8ce058dd009175069ac2d
Blixa Morgan <blixa@blixa.(none)>
2013-02-23 15:35:58 -0600

    added serial with port option

--- communications.py
+++ communications.py
@@ -1,6 +1,17 @@
 import time
-def SprayChalk():
-  print "Chalk on";
-  time.sleep(.01);
-  print "Chalk Off";
+import serial
 
+def init(port):
+  inited=1;
+  chalk=serial.Serial(port);
+  chalk.open();
+
+def SprayChalk():
+  if(inited):
+    print "Chalk on";
+    chalk.write('H');
+    time.sleep(.01);
+    print "Chalk Off";
+    chalk.write('L');
+  else:
+    print "No port opened";
